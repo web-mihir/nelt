f1afc2f77767b02079b7c5a7cda788a0b7ab8852
mroy35034 <mroy35034@gmail.com>
2022-04-06 15:31:08 +0600

    installed react router dom version 6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { createContext } from 'react';
 import './App.css';
-
+import {useApi} from './hooks/useApi';
 function App() {
+  const url = "https://fakestoreapi.com/products";
+  const [data] = useApi(url);
+  console.log(data);
   const contextProvider = createContext();
   return (
     <div className="App">
-        
+
     </div>
   );
 }
